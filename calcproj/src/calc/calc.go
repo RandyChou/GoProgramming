7fde756f841fe84d54b5a201ca6a733ab8c6d000
ZhouXiuLiang <zxl4189@qq.com>
2013-03-31 17:18:02 +0800

    第一章计算器例子

---
+++ calcproj/src/calc/calc.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"simplemath"
+	"strconv"
+)
+var Usage = func() {
+	fmt.Printf("USAGE: calc command [arguments] ...")
+	fmt.Printf("\nThe commands are:\n\tadd\tAddition of two values.\n\tsqrt\tSquare root of a non-negative value.")
+}
+
+func main() {
+	args := os.Args
+	if args == nil || len(args) < 3 {
+		fmt.Println("len(args) are ", len(args))
+		Usage()
+		return
+	}
+
+	switch args[1] {
+	case "add":
+		if len(args) != 4 {
+			fmt.Println("USAGE: calc add <integer1><integer2>")
+			return
+		}
+		v1, err1 := strconv.Atoi(args[2])
+		v2, err2 := strconv.Atoi(args[3])
+		if err1 != nil || err2 != nil {
+			fmt.Println("USAGE: calc add <integer1><integer2>")
+			return
+		}
+		ret := simplemath.Add(v1, v2)
+		fmt.Println("Result:", ret)
+	case "sqrt":
+		if len(args) != 3 {
+			fmt.Println("USAGE: calc sqrt <integer>")
+			return
+		}
+		v, err := strconv.Atoi(args[2])
+		if err != nil {
+			fmt.Println("USAGE: calc sqrt <integer>")
+			return
+		}
+		ret := simplemath.Sqrt(v)
+		fmt.Println("Result: ", ret)
+	default:
+		fmt.Println("len(args) are ", len(args))
+		Usage()
+	}
+}
---
+++ calcproj/src/simplemath/add_test.go
@@ -0,0 +1,12 @@
+package simplemath
+
+import (
+	"testing"
+)
+
+func TestAdd1(t *testing.T) {
+	r := Add(1, 2)
+	if r != 3 {
+		t.Errorf("Add(1, 2) failed. God %d, expected 3", r)	
+	}
+}
