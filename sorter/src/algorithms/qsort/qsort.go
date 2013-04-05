59a2b0ebbe82036dab914d4e7c94f20023ce3c1c
ZhouXiuLiang <zxl4189@qq.com>
2013-04-06 00:18:08 +0800

    增加自动编绎配置文件，直接运行此文件即可把整个项目编绎出来，可不设GOPATH环境变量

--- sorter/src/algorithms/qsort/qsort.go
+++ sorter/src/algorithms/qsort/qsort.go
@@ -1,40 +1,40 @@
 package qsort
 
 func quicSort(values []int, left, right int) {
-	temp := values[left]
-	p := left
-	i, j := left, right
+    temp := values[left]
+    p := left
+    i, j := left, right
 
-	for i <= j {
-		for j >= p && values[j] >= temp {
-			j--
-		}
+    for i <= j {
+        for j >= p && values[j] >= temp {
+            j--
+        }
 
-		if j >= p {
-			values[p] = values[j]
-			p = j
-		}
+        if j >= p {
+            values[p] = values[j]
+            p = j
+        }
 
-		if values[i] <= temp && i <= p {
-			i++
-		}
+        if values[i] <= temp && i <= p {
+            i++
+        }
 
-		if i <= p {
-			values[p] = values[i]
-			p =i
-		}
-	}
+        if i <= p {
+            values[p] = values[i]
+            p = i
+        }
+    }
 
-	values[p] = temp
-	if p - left > 1 {
-		quicSort(values, left, p - 1)
-	}
+    values[p] = temp
+    if p-left > 1 {
+        quicSort(values, left, p-1)
+    }
 
-	if right - p > 1 {
-		quicSort(values, p + 1, right)
-	}
+    if right-p > 1 {
+        quicSort(values, p+1, right)
+    }
 }
 
 func QuickSort(values []int) {
-	quicSort(values, 0, len(values) - 1)
+    quicSort(values, 0, len(values)-1)
 }
--- sorter/src/sorter/sorter.go
+++ sorter/src/sorter/sorter.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"bufio"
-	"flag"
-	"fmt"
-	"io"
-	"os"
-	"strconv"
-	"time"
+    "bufio"
+    "flag"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "time"
 
-	"algorithms/bubblesort"
-	"algorithms/qsort"
+    "algorithms/bubblesort"
+    "algorithms/qsort"
 )
 
 var infile *string = flag.String("i", "unsorted.dat", "File contains values for sorting")
@@ -18,89 +18,89 @@ var outfile *string = flag.String("o", "qsort", "File to receive sorted values")
 var algorithm *string = flag.String("a", "qsort", "Sort algorithm")
 
 func readValues(infile string) (values []int, err error) {
-	file, err := os.Open(infile)
-	if err != nil {
-		fmt.Println("Failed to open the input file ", infile)
-		return
-	}
+    file, err := os.Open(infile)
+    if err != nil {
+        fmt.Println("Failed to open the input file ", infile)
+        return
+    }
 
-	defer file.Close()
+    defer file.Close()
 
-	br := bufio.NewReader(file)
+    br := bufio.NewReader(file)
 
-	values = make([]int, 0)
+    values = make([]int, 0)
 
-	for {
-		line, isPrefix, err1 := br.ReadLine()
+    for {
+        line, isPrefix, err1 := br.ReadLine()
 
-		if err1 != nil {
-			if err1 != io.EOF {
-				err = err1
-			}
-			break
-		}
+        if err1 != nil {
+            if err1 != io.EOF {
+                err = err1
+            }
+            break
+        }
 
-		if isPrefix {
-			fmt.Println("A too long line, seems unexpected.")
-			return
-		}
+        if isPrefix {
+            fmt.Println("A too long line, seems unexpected.")
+            return
+        }
 
-		str := string(line)
+        str := string(line)
 
-		value, err1 := strconv.Atoi(str)
+        value, err1 := strconv.Atoi(str)
 
-		if err1 != nil {
-			err = err1
-			return
-		}
+        if err1 != nil {
+            err = err1
+            return
+        }
 
-		values = append(values, value)
-	}
+        values = append(values, value)
+    }
 
-	return
+    return
 }
 
 func writeValues(values []int, outfile string) error {
-	file, err := os.Create(outfile)	
-	if err != nil {
-		fmt.Println("Failed to create the output file ", outfile)
-		return err
-	}
+    file, err := os.Create(outfile)
+    if err != nil {
+        fmt.Println("Failed to create the output file ", outfile)
+        return err
+    }
 
-	defer file.Close()
+    defer file.Close()
 
-	for _, value := range values {
-		str := strconv.Itoa(value)
-		file.WriteString(str + "\r\n")
-	}
+    for _, value := range values {
+        str := strconv.Itoa(value)
+        file.WriteString(str + "\r\n")
+    }
 
-	return nil
+    return nil
 }
 
 func main() {
-	flag.Parse()
+    flag.Parse()
 
-	if infile != nil {
-		fmt.Println("infile =", *infile, "outfile =", *outfile, "algorithm =", *algorithm)
-	}
+    if infile != nil {
+        fmt.Println("infile =", *infile, "outfile =", *outfile, "algorithm =", *algorithm)
+    }
 
-	values, err := readValues(*infile)
-	if err == nil {
-		t1 := time.Now()
-		switch *algorithm {
-		case "qsort":
-			qsort.QuickSort(values)
-		case "bubblesort":
-			bubblesort.BubbleSort(values)
-		default:
-			fmt.Println("Sorting algorithm ", *algorithm, " is either unknown or unsupported.")
-		}
-		t2 := time.Now()
+    values, err := readValues(*infile)
+    if err == nil {
+        t1 := time.Now()
+        switch *algorithm {
+        case "qsort":
+            qsort.QuickSort(values)
+        case "bubblesort":
+            bubblesort.BubbleSort(values)
+        default:
+            fmt.Println("Sorting algorithm ", *algorithm, " is either unknown or unsupported.")
+        }
+        t2 := time.Now()
 
-		fmt.Println("The sorting process costs ", t2.Sub(t1), " to complete.")
+        fmt.Println("The sorting process costs ", t2.Sub(t1), " to complete.")
 
-		writeValues(values, *outfile)
-	} else {
-		fmt.Println(err)
-	}
+        writeValues(values, *outfile)
+    } else {
+        fmt.Println(err)
+    }
 }
